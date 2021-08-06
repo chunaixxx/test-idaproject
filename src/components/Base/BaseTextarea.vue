cceb24a0d0fb698c5c4e554c4e159ad2d67a2e23
Daniil <chunaixxx@gmail.com>
2021-08-06 09:42:14 +0300

    ADD: BaseTextarea component

---
+++ src/components/Base/BaseTextarea.vue
@@ -0,0 +1,31 @@
+<template>
+	<textarea type="text" class="baseTextArea" @input="updateValue" />
+</template>
+
+<script>
+export default {
+	name: 'base-textarea',
+
+	props: {
+		modelValue: String
+	},
+
+	emits: ['update:modelValue'],
+
+	methods: {
+		updateValue(event) {
+			this.$emit('update:modelValue', event.target.value)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import './BaseMixin.scss';
+
+.baseTextArea {
+	@include baseFormElements;
+
+	resize: none;
+}
+</style>
