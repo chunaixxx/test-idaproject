eb7f9276f832138a72973257dfb6f2fe5e018b4f
Daniil <chunaixxx@gmail.com>
2021-08-06 22:48:29 +0300

    ADD: error style for base input

--- src/components/Base/BaseInput.vue
+++ src/components/Base/BaseInput.vue
@@ -1,10 +1,20 @@
 <template>
-	<input
-		type="text"
-		class="baseInput"
-		@input="updateValue"
-		:value="modelValue"
-	/>
+	<div class="base-input-wrapper">
+		<input
+			type="text"
+			class="base-input"
+			@input="updateValue"
+			:value="modelValue"
+			:class="{ 'base-input--error': !valid }"
+			v-bind="$attrs"
+		/>
+
+		<transition name="transform">
+			<span v-if="!valid" class="base-input-error">
+				Поле является обязательным
+			</span>
+		</transition>
+	</div>
 </template>
 
 <script>
@@ -12,7 +22,11 @@ export default {
 	name: 'base-input',
 
 	props: {
-		modelValue: String
+		modelValue: String,
+		valid: {
+			type: Boolean,
+			default: true
+		}
 	},
 
 	emits: ['update:modelValue'],
@@ -26,9 +40,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/styles/_variables.scss';
 @import './BaseMixin.scss';
 
-.baseInput {
+.base-input-wrapper {
+	position: relative;
+}
+
+.base-input {
 	@include baseFormElements;
+	transition: border 0.25s ease;
+	width: 100%;
+
+	&--error {
+		border: 1px solid $error;
+	}
+}
+
+.base-input-error {
+	font-size: 8px;
+	color: $error;
+	position: absolute;
+	top: calc(100% + 4px);
+	left: 0;
+}
+
+.transform-enter-active,
+.transform-leave-active {
+	transition: all 0.5s ease;
+}
+
+.transform-enter-from,
+.transform-leave-to {
+	opacity: 0;
+	transform: translateX(10px)
 }
 </style>
