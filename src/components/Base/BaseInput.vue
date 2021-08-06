4aa6ef0566e15b62b41062888a3ce5a6de9cb488
Daniil <chunaixxx@gmail.com>
2021-08-06 09:39:26 +0300

    ADD: BaseInput component

---
+++ src/components/Base/BaseInput.vue
@@ -0,0 +1,29 @@
+<template>
+	<input type="text" class="baseInput" @input="updateValue" />
+</template>
+
+<script>
+export default {
+	name: 'base-input',
+
+	props: {
+		modelValue: String
+	},
+
+	emits: ['update:modelValue'],
+
+	methods: {
+		updateValue(event) {
+			this.$emit('update:modelValue', event.target.value)
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+@import './BaseMixin.scss';
+
+.baseInput {
+	@include baseFormElements;
+}
+</style>
