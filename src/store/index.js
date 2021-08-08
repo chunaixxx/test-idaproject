ce2ae6c55f841178544edfeacf17f7a3d9cff88a
Daniil <chunaixxx@gmail.com>
2021-08-08 04:03:26 +0300

    ADD: vuex storage for goods

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
 import { createStore } from 'vuex'
 
 export default createStore({
-	state: {},
-	mutations: {},
-	actions: {},
-	modules: {}
+	state: {
+		goods: []
+	},
+	mutations: {
+		addGood(state, payload) {
+			state.goods.push({
+				id: Date.now(),
+				...payload
+			})
+		},
+
+		deleteGood(state, id) {
+			state.goods = state.goods.filter(item => item.id !== id)
+		}
+	}
 })
